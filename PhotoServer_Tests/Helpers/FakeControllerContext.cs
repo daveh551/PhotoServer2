3c0b97ae9d3adff1822165f4c8ddf99ce4424aed
Dave Hanna <dave@infotouchsys.com>
2014-01-09 16:43:52 -0600

    Move FakeControllerContext from TestSupport to Tests; remove ValuesController

---
+++ PhotoServer_Tests/Helpers/FakeControllerContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using System.Web.Http.Controllers;
+using MediaTypeFormatters;
+
+namespace PhotoServer_Tests
+{
+    public class FakeControllerContext : HttpControllerContext
+    {
+        public FakeControllerContext()
+        {
+            Configuration = new HttpConfiguration();
+            Configuration.Formatters.Add(new JpegMediaTypeFormatter());
+            // Setup configuration with routes, etc. as per application
+            PhotoServer2.WebApiConfig.Register(Configuration);
+
+
+        }
+
+
+
+    }
+
+
+
+}
